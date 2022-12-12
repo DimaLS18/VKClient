28689cf734522224df99f4e1d282f02163b2af31
DimaLS18 <kovrigindima@gmail.com>
2022-12-12 11:13:30 +0700

    выполнил дз

---
+++ VKClientRM/VKClientRM/Operation/ParseDataOperation.swift
@@ -0,0 +1,24 @@
+//
+//  ParseDataOperation.swift
+//  VKClientRM
+
+
+import Foundation
+
+ /// Асинхронный парсинг данных
+ final class ParseDataOperation: Operation {
+     // MARK: - Public Properties
+
+     var itemPersons: [ItemPerson] = []
+
+     // MARK: - Public Methods
+
+     override func main() {
+         guard
+             let getDataOperation = dependencies.first as? GetDataOperation,
+             let data = getDataOperation.data,
+             let itemsPerson = try? JSONDecoder().decode(Person.self, from: data).response.items
+         else { return }
+         itemPersons = itemsPerson
+     }
+ }
